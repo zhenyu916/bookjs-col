d2bd4d6bb82c942c976ea7693915bb709133564d
zhenyu916 <guozhenyu999@gmail.com>
2024-05-28 15:03:47 +0800

    分栏2

--- dist/mock.js
+++ dist/mock.js
@@ -1,18 +1,18 @@
 const data = []
 
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 3; i++) {
     const oneData = {}
     oneData.categoryName = `一级-${i}`
     oneData.children = []
-    for (let j = 0; j < 10; j++) {
+    for (let j = 0; j < 6; j++) {
         const twoData = {}
         twoData.categoryName = `二级-${j}`
         twoData.children = []
-        for (let k = 0; k < 10; k++) {
+        for (let k = 0; k < 6; k++) {
             const threeData = {}
             threeData.categoryName = `三级-${k}`
             threeData.data = []
-            for (let m = 0; m < 10; m++) {
+            for (let m = 0; m < 6; m++) {
                 const p = {
                     "productName": `FuelInjector 4010642 For Cummins Engine K38 KTA38_0`,
                     "comparible": `For BOBCAT`,
